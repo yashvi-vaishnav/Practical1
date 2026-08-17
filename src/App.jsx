279d99670669410bbebcebb2def07772aba0aec3
yashvi-vaishnav <yashvivaishnav866@gmail.com>
2026-08-17 17:16:38 +0530

    PRACTICAL1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+return (
+<div>
+<h1>Welcome to React</h1>
+<h2>My First React Application</h2>
+<p>
+This is a simple React application created using JSX and functional
+components.
+</p>
+<p>React makes it easy to build user interfaces.</p>
+</div>
+);
+}
+
+export default App;
